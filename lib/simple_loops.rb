1663b49ed79041586868f1604fd8fc46aa1ac5d3
Luis Torres <luistjr@gmail.com>
2020-09-25 19:32:02 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,40 @@
 # Write your methods here
+
+def loop_message_five_times(message)
+  counter = 0 
+  times = 5
+  while counter < times do 
+    puts message
+    counter += 1
+  end 
+end 
+
+def loop_message_n_times(message, integer)
+  counter = 0 
+  while counter < integer do 
+    puts message
+    counter += 1 
+  end 
+end
+
+array = ["All great achievements", "require time", "- Maya Angelou"]
+
+def output_array(array)
+  counter = 0 
+  while array[counter] do 
+    puts array[counter]
+    counter += 1 
+  end 
+end 
+
+def return_string_array(array)
+  counter = 0 
+  while array[counter] do 
+    array[counter].to_s 
+    counter += 1
+  end 
+  return array
+end
+
+return_string_array(array)
+
